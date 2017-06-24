69c74fa20cb32e5194d414b5feeaaf2e843eb74d
gititgoro <justinheyns@gmail.com>
2017-06-24 23:38:43 +0200

    added todos and reminders

--- Pythia.sol
+++ Pythia.sol
@@ -1,7 +1,12 @@
 pragma solidity ^0.4.11;
 //TODO: see common patters for restricting access so that bids can't be inspected without going through request function
 //TODO: read security stuff
-
+//TODO: when placing a bid, do an inplace sort so that the final request doesn't run out of gas
+//SECURITY: Always change all variables and only as the very last act, transfer ether.
+//SECURITY: perform all bookkeeping on the same scope level in case malicious actor gets stack to 1024 and then you try to do something with function call and 
+//it isn't atomic
+//SECURITY: use invariants to trigger safe mode if any of the invariants become inconsistent.
+//Breakthrough: It doesn't matter than the state is viewable to all. What matters is that it's private to contracts.
 import "./AccessRestriction.sol";
 
 contract Pythia is AccessRestriction{
@@ -23,6 +28,8 @@ contract Pythia is AccessRestriction{
     address sender;
     }
 
-    mapping(string => Kreshmoi) bids;
-    mapping(address => Kreshmoi[]) successfulHistory; //TODO: make sure dynamic array
+    mapping (string => Kreshmoi) bids;
+    mapping (address => Kreshmoi []) successfulHistory; //TODO: make sure dynamic array
+    //if a datafeed is requested and doesn't exist, ("name",false) is created, otherwise ("name",true) is set
+    mapping (string => bool) existentDataFeeds; 
 }
