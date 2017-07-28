673d4217230f05ae4aa9fe349c2f69ce2e5d1364
gititgoro <justinheyns@gmail.com>
2017-07-28 23:17:46 +0200

    refactored test to initially submit a prophecy

--- test/pythia.js
+++ test/pythia.js
@@ -5,16 +5,26 @@ contract('Pythia', function (accounts) {
     var ethzarProphecyLength;
     var fistAccount = accounts[0];
     var secondAccount = accounts[1];
+    var submitResult;
 
-
-    it("should submit 1 integer prophecy to ETHZAR", () => {
+    before(() => {
         return Pythia.deployed().then(instance => {
             PythiaInstance = instance;
             return instance.SubmitIntegerProphecy("ETHZAR", 120, { from: secondAccount });
-        }).then(result => {
-            var logs = result.logs;
-            assert.equal(logs.length, 1, "There should only be one event log");
+        })
+        .then(result => submitResult = result);
+    });
 
-        });
+    it("should submit 1 integer prophecy to ETHZAR", () => {
+        var logs = submitResult.logs;
+        assert.equal(logs.length, 1, "There should only be one event log");
+        var log = logs[0];
+
+        assert.equal(log.event, "ProphecySubmission");
+        assert.isTrue(log.args != null);
+        assert.equal(Object.keys(log.args).length, 2, "args length should equal 2");
+
+        assert.equal(log.args.from, secondAccount);
+        assert.equal(log.args.datafeed, "ETHZAR");
     });
 });
