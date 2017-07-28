2a3ada07f41d54d247ba1d650ba96734dd2ecf51
gititgoro <justinheyns@gmail.com>
2017-07-28 22:29:36 +0200

    Converted to truffle

--- test/pythia.js
+++ test/pythia.js
@@ -3,21 +3,18 @@ var Pythia = artifacts.require("../contracts/Pythia.sol");
 contract('Pythia', function (accounts) {
     var PythiaInstance;
     var ethzarProphecyLength;
+    var fistAccount = accounts[0];
+    var secondAccount = accounts[1];
+
 
     it("should submit 1 integer prophecy to ETHZAR", () => {
         return Pythia.deployed().then(instance => {
             PythiaInstance = instance;
-        });
-        var eventResult;
-        var eventFired = false;
-        instance.watch((error, response) => {
-            eventResult = response;
-            assert.equal(eventResult, "horse");
-            eventFired=true;
-        });
+            return instance.SubmitIntegerProphecy("ETHZAR", 120, { from: secondAccount });
+        }).then(result => {
+            var logs = result.logs;
+            assert.equal(logs.length, 1, "There should only be one event log");
 
-        instance.SubmitIntegerProphecy("ETHZAR", 120);
-        while(!eventFired);
-        
+        });
     });
 });
