3806f1ebf6e3148fcbc42f717c3611fed303c5cc
gititgoro <justinheyns@gmail.com>
2017-07-31 18:09:31 +0200

    Re-write because I previously failed to model pythia on the fact that transactions cost ether but return nothing and calls are free and return value types.

--- contracts/Pythia.sol
+++ contracts/Pythia.sol
@@ -5,196 +5,65 @@ pragma solidity ^0.4.11;
 //SECURITY: perform all bookkeeping on the same scope level in case malicious actor gets stack to 1024 and then you try to do something with function call and 
 //it isn't atomic
 //SECURITY: use invariants to trigger safe mode if any of the invariants become inconsistent.
-//Breakthrough: It doesn't matter than the state is viewable to all. What matters is that it's private to contracts.
 //TODO: add modifier to allow owner to disable the entire contract in case I want to launch a newer version.
 //TODO: make a withdrawal function
+/*Domain language: Post bounty, OfferKreshmoi, Reward bounty, 
+Collect bounty reward, Successful kreshmoi
+*/
 import "./AccessRestriction.sol";
 
 contract Pythia is AccessRestriction{
-
-    enum KreshmoiDataType{
-        INT,MICRO
-    }
 
     function Pythia(){
         
     }
 
     //A kreshmoi is an ancient Greek word meaning an utterance issued by an oracle.
-    //In our decentralized oracle, Pythia, Kreshmoi will be the name of the data structure representing an "utterance" on a given datafeed.
+    //In our decentralized oracle, Pythia, Kreshmoi will be the name of the data structure 
+    //representing a successful "utterance" on a given datafeed.
     struct Kreshmoi{
-    uint blockNumber;
-    KreshmoiDataType dataType;
-    int32 value_int;
-    int64 value_micro; // each unit of this represents 1 millionth of a unit
-   
-    address sender;
+    uint16 blockRange;
+    uint8 decimalPlaces;// floats don't exist in solidity (yet)
+    int64 value;
+    uint8 sampleSize;
+    uint valueRange;
+    address bountyPoster;
+    }
+
+    struct Bounty{
+        uint16 maxBlockRange;
+        uint earliestBlock;
+        uint weiRewardPerOracle;
+        uint8 requiredSampleSize;
+        uint maxValueRange;
+        address[] oracles;
+        int64 [] predictions;
+        uint8 decimalPlaces;
     }
 
     mapping (address => uint) rewardForSuccessfulProphecies; //stores the ether value of each successful kreshmoi. And yes, you can forget about reentrancy attacks.
     mapping (string => Kreshmoi[]) prophecies; //key is USDETH or CPIXZA for instance
-    mapping (address => uint64) successfulHistory; //TODO: make sure dynamic array
-    mapping (address => mapping (string => int32)) latestIntResult;
-    mapping( address => mapping(string => bool)) intResultExists;
+    mapping (string => Bounty[]) openBounties; 
 
-    function SubmitIntegerProphecy(string feedName, int32 value ) notBlacklisted returns (string result){
-        uint length = prophecies[feedName].length;
-        if(length >0 && prophecies[feedName][length-1].dataType!=KreshmoiDataType.INT){
-            result = "invalid datatype for feed. Expected decimal but was int";
-            }
+    function PostBounty(string datafeed, uint16 maxBlockRange,uint maxValueRange,uint8 requiredSampleSize,uint8 decimalPlaces) payable{
+        if(msg.value/requiredSampleSize<1)
+            throw;
+        
+        openBounties[datafeed].push(Bounty({
+            maxBlockRange:maxBlockRange,
+            maxValueRange:maxValueRange,
+            weiRewardPerOracle:msg.value/requiredSampleSize,
+            requiredSampleSize:requiredSampleSize,
+            decimalPlaces:decimalPlaces,
+            predictions: new int64[](0),
+            oracles: new address [](0),
+            earliestBlock:0
+        }));
 
-        result = "success";
-        prophecies[feedName].length+=1;
-        prophecies[feedName][length].blockNumber = block.number;
-        prophecies[feedName][length].value_int =value;
-        prophecies[feedName][length].dataType =KreshmoiDataType.INT;
-        prophecies[feedName][length].sender =msg.sender;
-
-        ProphecySubmission(msg.sender,feedName);
-    }
-
-   function SubmitDecimalProphecy(string feedName, int64 value ) notBlacklisted returns (string result){
-        uint length = prophecies[feedName].length;
-           if(length >0 && prophecies[feedName][length-1].dataType!=KreshmoiDataType.INT){
-            result = "invalid datatype for feed. Expected int but was decimal";
-            }     
-        result = "success";
-
-        prophecies[feedName].length+=1;
-        prophecies[feedName][length].blockNumber = block.number;
-        prophecies[feedName][length].value_micro =value;
-        prophecies[feedName][length].dataType =KreshmoiDataType.MICRO;
-        prophecies[feedName][length].sender =msg.sender;
-
-        ProphecySubmission(msg.sender,feedName);
-    }
-
-    function ValidateProphecyLength(string feedName, uint8 maxSampleSize, uint weiAmount) returns (uint8){
-        uint8 prophecyLength = prophecies[feedName].length>255?255:uint8(prophecies[feedName].length);
-        if(prophecyLength==0 || maxSampleSize>50 || weiAmount<maxSampleSize) throw;//last condition because each winner should get at least 1 wei  
-        return prophecyLength;
+        BountyPosted (msg.sender,datafeed);
     }
     
-    function GetIntResult(string feedName) returns (int32){
-        if(intResultExists[msg.sender][feedName])
-            return latestIntResult[msg.sender][feedName];
-        throw;
-    }
-
-    function RequestInteger(string feedName, uint8 maxSampleSize, int32 acceptableRange, uint16 minSuccesses) payable {
-       uint8 prophecyLength = ValidateProphecyLength(feedName,maxSampleSize,msg.value); //TODO: this is failing
-        
-       uint8 actualSampleSize = (block.number- prophecies[feedName][prophecyLength -1].blockNumber)>255?255:
-            uint8(block.number- prophecies[feedName][prophecyLength -1].blockNumber);
-
-        Kreshmoi[] memory filtered = Filter(prophecies[feedName],actualSampleSize,filterLatest);
-                            filtered = Reduce(filtered,FilterOutDuplicateUsers); 
-                            filtered = ThresholdMinSuccesses(filtered, minSuccesses);   
-        
-        if(GetRange(filtered)>acceptableRange)
-            throw;
-            
-        int32 result = 0;
-        for(uint i = 0; i<(filtered.length>255?255:filtered.length); i++){
-            successfulHistory[filtered[i].sender]++;
-            result = result + filtered[i].value_int;
-        }
-        result = result/int32(filtered.length);
-
-        uint rewardPerWinner = msg.value / filtered.length; //rewardperWinner
-
-        latestIntResult[msg.sender][feedName] = result;
-        intResultExists[msg.sender][feedName] = true;
-        for(i=0;i<filtered.length;i++){
-            rewardForSuccessfulProphecies[filtered[i].sender]+=rewardPerWinner;
-        }
-    }
-
-    function GetRange(Kreshmoi [] memory kreshmoi) internal returns (int32){
-        int32[] memory biggestSmallest = new int32[](2);
-        for(uint i =0;i<kreshmoi.length;i++){
-            if(kreshmoi[i].value_int<biggestSmallest[0]){
-                biggestSmallest[0] = kreshmoi[i].value_int;
-            }
-            if(kreshmoi[i].value_int>biggestSmallest[1]){
-                biggestSmallest[1] = kreshmoi[i].value_int;
-            }
-        }
-        return biggestSmallest[1] - biggestSmallest[0];
-    }
-
-    function ThresholdMinSuccesses(Kreshmoi [] memory initialKreshmoi, uint16 minSuccesses) internal returns (Kreshmoi[] memory){
-        bool [] memory valid = new bool[] (initialKreshmoi.length);
-        uint validCount  = 0;
-        for(uint i =0;i<valid.length;i++){
-            if(successfulHistory[initialKreshmoi[i].sender]>=minSuccesses)
-                {
-                    valid[i]= true;
-                    validCount++;
-                }
-        }
-
-        Kreshmoi[] memory threshold = new Kreshmoi[](validCount);
-        validCount = 0;
-        for(i =0;i<initialKreshmoi.length;i++){
-            if(valid[i]){
-                threshold[validCount]= initialKreshmoi[i];
-                validCount++;
-            }
-        }
-        return threshold;
-    }
-
-    function filterLatest (Kreshmoi memory kreshmoi, uint8 blockAge) internal returns (bool){
-            return((block.number- kreshmoi.blockNumber)>255?255:
-                uint8(block.number- kreshmoi.blockNumber))>blockAge;
-    }
-
-    function FilterOutDuplicateUsers(Kreshmoi[] memory accumulator, Kreshmoi memory current) internal returns (Kreshmoi[] memory){  
-       Kreshmoi [] memory potentialNewAccumulator = new Kreshmoi[](accumulator.length+1);
-        for(uint i =0;i<accumulator.length;i++){
-            if(current.sender == accumulator[i].sender)
-                return accumulator; 
-                potentialNewAccumulator[i] = accumulator[i];
-        }
-        potentialNewAccumulator[accumulator.length] = current;
-        return potentialNewAccumulator;
-    }
-
-    function Reduce (Kreshmoi[] initialArray, function (Kreshmoi [] memory ,Kreshmoi memory) returns (Kreshmoi [] memory) reducer) internal returns(Kreshmoi[]){
-      Kreshmoi [] memory accumulator = new Kreshmoi[](0);
-        for(uint i =0;i<initialArray.length;i++){
-              accumulator = reducer(accumulator, initialArray[0]);
-        }
-        return accumulator;
-    }
-
-    function Filter(Kreshmoi[] memory kreshmoi,uint8 initialValue, function(Kreshmoi memory, uint8) returns (bool) predicate ) internal returns (Kreshmoi[]){
-             bool[] memory chosen = new bool[](kreshmoi.length);
-             uint8 actualLength =0;
-
-            for(uint i =0; i<kreshmoi.length;i++){
-                if(predicate(kreshmoi[i],initialValue)){
-                    chosen[i] = true;
-                    actualLength++;
-                }
-            }
-            
-            Kreshmoi[] memory actual =new Kreshmoi[](actualLength);
-            actualLength=0;
-            for(i =0;i<chosen.length;i++){
-                if(chosen[i]){
-                    actual[actualLength++] = kreshmoi[i];
-                }
-            }
-            return actual;
-        }
-
-    modifier notBlacklisted() { //wallet or contract permanently blacklisted. If this was a mistake the user will have to start their own pythia with blackjack and hookers.
-          require(successfulHistory[msg.sender] >=0);
-        _;
-    }
-
+    event BountyPosted (address from, string datafeed);
     event ProphecySubmission(address from, string datafeed );
 
 }
--- test/pythia.js
+++ test/pythia.js
@@ -7,42 +7,22 @@ contract('Pythia', function (accounts) {
     var secondAccount = accounts[1];
     var submitResult;
 
+    var fixtureBounty = {
+                maxBlockRange: 10,
+                weiRewardPerOracle: 100,
+                RequiredSampleSize: 4,
+                maxValueRange: 13,
+                decimalPlaces:2
+            };
+
     before(() => {
         return Pythia.deployed().then(instance => {
             PythiaInstance = instance;
-            return instance.SubmitIntegerProphecy("ETHZAR", 120, { from: secondAccount });
+
+            return instance.PostBounty("ETHZAR", bounty.maxBlockRange,
+                bounty.maxValueRange, bounty.RequiredSampleSize, bounty.decimalPlaces, { from: secondAccount, value:"10000000" });
         })
             .then(result => submitResult = result);
-    });
-
-    it("should submit 1 integer prophecy to ETHZAR", () => {
-        var logs = submitResult.logs;
-        assert.equal(logs.length, 1, "There should only be one event log");
-        var log = logs[0];
-
-        assert.equal(log.event, "ProphecySubmission");
-        assert.isTrue(log.args != null);
-        assert.equal(Object.keys(log.args).length, 2, "args length should equal 2");
-
-        assert.equal(log.args.from, secondAccount);
-        assert.equal(log.args.datafeed, "ETHZAR");
-    });
-
-    it("should validate prophecy", () => {
-        return PythiaInstance.ValidateProphecyLength.call("ETHZAR", 12, 100)
-            .then(prophecyLength => {
-                assert.equal(prophecyLength, 1, "There should only be one prophecy submitted");
-            });
-    });
-
-    it("RequestInteger should create result", () => {
-        return PythiaInstance.RequestInteger("ETHZAR", 10, 10, 0, { from: firstAccount })
-            .then(receipt => {
-
-                return PythiaInstance.GetIntResult.call("ETHZAR", { from: firstAccount });
-            }).then(intResult => {
-                assert.equal(intResult, 120);
-            });
     });
 
 });
