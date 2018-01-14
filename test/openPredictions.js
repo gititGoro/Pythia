dca2838e5b40b4a7cd970788aaf91fc2ebcfbfb6
gititgoro <>
2018-01-14 11:03:54 +0200

    updated tests. Attempting to switch over to typescript next. Wish me luck.

--- test/openPredictions.js
+++ test/openPredictions.js
@@ -26,21 +26,9 @@ contract('OpenPredictions', accounts => {
             .catch(error=>done(error));
     });
 
-    test("run before", async () => {
-        console.log(accounts[1]);
-    });
-
-
-   
     test("place Prediction at valid feed", async () => {
         var BTCUSDID_int = parseInt(BTCUSDID);
         await openPredictionsInstance.placePrediction(BTCUSDID, 1300, { from: accounts[2] });
     });
-
-    // test("get prediction for valid feed", async () => {
-    //     var predictions = await openPredictionsInstance.getPredictionsByFeedIdSinceBlock.call(0, 0);
-    //     console.log(JSON.stringify(predictions));
-
-    // });
 
 });
