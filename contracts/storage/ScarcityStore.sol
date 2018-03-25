47b610f686a258a773f0cbe4997d25689b605d1d
gititgoro <justinheyns@gmail.com>
2018-03-26 00:20:13 +0200

    split scarcity into storage and functionality but error persists. After commenting out most code, still 'out of gas' when migrating. Truffle bug alas.

--- contracts/storage/ScarcityStore.sol
+++ contracts/storage/ScarcityStore.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.17;
 import "../AccessRestriction.sol";
+import "../libraries/SafeMath.sol";
 
 contract ScarcityStore is AccessRestriction {
-
+    using SafeMath for uint;
     mapping (address => uint) public balances;
     //the following 3 variables allow scarcity balances to be scaled.
     //if the supply falls too low, a new factor is set. All users are frozen
@@ -11,13 +12,45 @@ contract ScarcityStore is AccessRestriction {
     mapping (address=>uint) public iteration;
     uint public currentLockTick;
     uint[] public multiplicativeFactors;
+    uint public supply;
+    function ScarcityStore () public {
+        supply = 100000000 * 10**uint(18);
+    }
 
-    function getMultiplicativeFactorLength() public returns (uint) {
+    function IncrementSupply(uint value) onlyOwner public {
+        supply = supply.add(value);
+    }
+    
+    function DecrementSupply(uint value) onlyOwner public {
+        supply = supply.sub(value);
+    }
+
+    function ResetOwnerBalance (address scarcityOwner) onlyOwner public {
+        balances[scarcityOwner] = supply;
+    }
+
+    function BalanceIncrement(address to, uint value) onlyOwner public {
+        balances[to] = balances[to].add(value);
+    }
+
+    function BalanceDecrement(address to, uint value) onlyOwner public {
+        balances[to] = balances[to].sub(value);
+    }
+
+    function getMultiplicativeFactorLength() public view returns (uint) {
         return multiplicativeFactors.length;
     }
 
     function pushMultiplicativeFactor(uint factor) onlyOwner public {
         multiplicativeFactors.push(factor);
+    }
+
+    function incrementLockTick () onlyOwner public {
+        currentLockTick = currentLockTick.add(1);
+    }
+
+    function incrementIteration(address sender) onlyOwner public {
+        iteration[sender] = iteration[sender].add(1); 
     }
 
     function () payable public { 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,20 +2,24 @@ var AccessRestriction = artifacts.require("AccessRestriction");
 var FeedMaster = artifacts.require("FeedMaster");
 var CircularBuffer = artifacts.require("CircularBufferLib");
 var SafeMath = artifacts.require("SafeMath");
+var ScarcityStore = artifacts.require("ScarcityStore");
 var Scarcity = artifacts.require("Scarcity");
 var PythiaBank = artifacts.require("PythiaBank");
 
 module.exports = function (deployer) {
-  deployer.deploy(AccessRestriction, { gas: 6721975 });
-  deployer.deploy(CircularBuffer, { gas: 6721975 });
-  deployer.deploy(SafeMath, { gas: 6721975 });
+  deployer.deploy(AccessRestriction);
+  deployer.deploy(CircularBuffer);
+  deployer.deploy(SafeMath);
   deployer.link(AccessRestriction, FeedMaster);
   deployer.link(AccessRestriction, Scarcity);
+  deployer.link(AccessRestriction, ScarcityStore);
   deployer.link(AccessRestriction, PythiaBank);
   deployer.link(SafeMath, FeedMaster);
-  deployer.link(SafeMath, Scarcity);
+  deployer.link(SafeMath, ScarcityStore);
   deployer.link(SafeMath, PythiaBank);
+
   deployer.deploy(FeedMaster, { gas: 6721975 });
   deployer.deploy(PythiaBank, { gas: 6721975 });
-  deployer.deploy(Scarcity, { gas: 6721975 });
+  deployer.deploy(ScarcityStore, { gas: 6721975 });
+  deployer.deploy(Scarcity);
 };
